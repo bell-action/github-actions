7a43c9df4bf8346ea9026ca9d144394c877a1496
bell-ho <62537935+bell-ho@users.noreply.github.com>
2024-04-12 15:45:34 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1-aws1 try3
+          Learn GithubAction cicd1-aws1 try5
         </a>
       </header>
     </div>
